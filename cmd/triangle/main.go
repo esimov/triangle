b8e5db4932b873be786223e689235f711b14b474
esimov <esimovaq@yahoo.com>
2020-04-20 14:19:35 +0300

    Removed accidentally included webp module

--- cmd/triangle/main.go
+++ cmd/triangle/main.go
@@ -12,7 +12,6 @@ import (
 	"time"
 
 	"github.com/esimov/triangle"
-	_ "golang.org/x/image/webp"
 )
 
 const helperBanner = `
@@ -36,7 +35,7 @@ func main() {
 		// Command line flags
 		source          = flag.String("in", "", "Source")
 		destination     = flag.String("out", "", "Destination")
-		blurRadius      = flag.Int("blur", 6, "Blur radius")
+		blurRadius      = flag.Int("blur", 4, "Blur radius")
 		sobelThreshold  = flag.Int("sobel", 10, "Sobel filter threshold")
 		pointsThreshold = flag.Int("points", 20, "Points threshold")
 		maxPoints       = flag.Int("max", 2500, "Maximum number of points")
